48657625327f6a4e834089e9885185c6e4aba96d
murtaza-khan <murtazakhancs@gmail.com>
2023-02-10 18:52:41 +0500

    add product service

--- grpc-nest-product-svc/src/app.module.ts
+++ grpc-nest-product-svc/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'containers-us-west-167.railway.app',
+      port: 7876,
+      database: 'railway',
+      username: 'postgres',
+      password: 'e24bcLnCAJcZLWo0VrLc',
+      entities: ['dist/**/*.entity.{ts,js}'],
+      synchronize: true, // never true in production!
+    }),
+    ProductModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
